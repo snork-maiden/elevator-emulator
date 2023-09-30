61b715816a00101d157cae737fd0253de8bb905a
snork-maiden <pollykoldysheva@gmail.com>
2023-09-30 16:37:27 +0300

    Base components

---
+++ src/components/HouseFloor.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ElevatorStop from './ElevatorStop.vue';
+
+defineProps({
+    floorNumber: Number,
+})
+
+function requestElevator(floor) {
+  // if(floor === currentFloor) return;
+  // if(goals.includes(floor)) return;
+  // goals.push(floor);
+}
+</script>
+
+<template>
+  <div class="floor">
+    <ElevatorStop></ElevatorStop>
+    <label class="button">
+      {{ floorNumber }}
+      <input type="radio" :value="floorNumber" @click="requestElevator(floorNumber)" checked/>
+    </label>
+  </div>
+</template>
+
+<style scoped>
+.floor {
+  display: flex;
+  align-items: center;
+}
+
+.button {
+  margin-left: 5em;
+}
+
+
+</style>
---
+++ src/components/TheHouse.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HouseFloor from './HouseFloor.vue';
+
+let floorsNumber = 5;
+</script>
+
+<template>
+<HouseFloor class="houses" :floorNumber="floor" v-bind:key="floor" v-for="floor in floorsNumber"/>
+</template>
+<style scoped>
+.houses {
+  display: flex;
+}
+</style>
